26e3f3394f96e260a5a84da960065926034f10ad
Tom Nolan <tsnolan@outlook.com>
2019-05-22 21:12:09 -0500

    feat: add additional info on clicking items

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => (
       Using a combination of React's memo, useReducer and useCallback, we can
       ensure only the items that changed below are actually re-rendered. Using
       the React Dev Tools's highlight updates feature, you can observe the
-      difference in re-renders below.
+      difference in re-renders below by clicking list items.
     </p>
   </div>
 );
